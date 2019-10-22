47acb9aa91e2873a7647f6f64dafcf8fb441ccbe
ezii <manu@korfmann.info>
2019-10-23 00:54:00 +0200

    ………

--- lib/ladder.rb
+++ lib/ladder.rb
@@ -28,7 +28,7 @@ class Ladder
     if arg.is_a?(Hash) && arg[:stop]
       @saved_blocks.each.with_index do |block, i|
         byebug
-        @saved_blocks.delete(i) if arg[:stop] == block
+        @saved_blocks.delete_at(i) if arg[:stop] == block
       end
     else
       if !arg.nil?
