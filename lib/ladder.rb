b683093d333797eb9672034d9b388bf837aa8130
ezii <manu@korfmann.info>
2019-10-22 16:22:31 +0200

    …Version:1.test...

--- lib/ladder.rb
+++ lib/ladder.rb
@@ -1,6 +1,51 @@
+RUBY_MAIN_OBJECT = self
 
-class Ladder
-  def self.hi
-    puts "Hello world!"
+
+module Ladder
+  module FOR_RUBY_MAIN_OBJECT
+    attr_accessor :singleton_ladder_instance
+    def ladder(arg = nil, &block)
+      self.singleton_ladder_instance ||= Ladder.new
+      
+      self.singleton_ladder_instance.ladder(arg, &block)
+    end
+  end
+  
+  module FOR_RUBY_OBJECT
+    def ladder(arg = nil, &block)
+      RUBY_MAIN_OBJECT.singleton_ladder_instance.ladder(arg, &block)
+    end
+    
+
+  end
+  
+  def ladder(arg = nil, &block)
+      
+    if !arg.nil?
+      
+      
+      #- 1
+      
+      
+      @saved_blocks.each { |block| block.call(arg) } 
+      
+      
+    else
+      
+      
+      #- 2
+      
+      
+      @saved_blocks ||= []
+      @saved_blocks.push(block)
+    end
   end
 end
+
+class Object
+  include Ladder::FOR_RUBY_OBJECT
+end
+
+extend Ladder::FOR_RUBY_MAIN_OBJECT
+
+ ladder
